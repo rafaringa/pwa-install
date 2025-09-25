e2ebe6c6f351b57fc0cb867b228a47a083e0ea66
Rafael Maia <71021601+rafaringa@users.noreply.github.com>
2025-09-25 08:59:07 -0300

    Refactor HTML structure in template.ts

--- src/templates/chrome/template.ts
+++ src/templates/chrome/template.ts
@@ -10,27 +10,27 @@ const template = (name: string, description: string, installDescription: string,
 
     return html`
         <div id="pwa-install-element" dir="${isRTL ? 'rtl' : 'ltr'}">
-            <div class="install-dialog chrome ${classMap(installDialogClasses())}">
-                <div class="dialog-body">
+            <div part="body" class="install-dialog chrome ${classMap(installDialogClasses())}">
+                <div part="dialog-body" class="dialog-body">
                     <div class="icon">
                         <img src="${icon}" alt="icon" class="icon-image" draggable="false">
                     </div>
-                    <div class="about">
-                        <div class="name">
+                    <div class="about" part="about">
+                        <div class="name" part="name">
                             <label>${name}</label>
                             ${!disableClose? html`<button class="material-button secondary close" @click='${hideDialog}'>
                                 <svg viewBox="0 0 24 24"><path d="M5.3 18.7c.2.2.4.3.7.3s.5-.1.7-.3l5.3-5.3 5.3 5.3a1.08 1.08 0 0 0 .7.3 1.08 1.08 0 0 0 .7-.3c.4-.4.4-1 0-1.4L13.4 12l5.3-5.3c.4-.4.4-1 0-1.4s-1-.4-1.4 0L12 10.6 6.7 5.3c-.4-.4-1-.4-1.4 0s-.4 1 0 1.4l5.3 5.3-5.3 5.3c-.4.4-.4 1 0 1.4z"/></svg>
                             </button>` : ''}
                         </div>
-                        <div class="hostname">${location.hostname}</div>
+                        <div class="hostname" part="hostname">${location.hostname}</div>
                         
                     </div>
-                    ${description ? html `<div class="description app-description">${description}</div>`: ''}
+                    ${description ? html `<div part="description" class="description app-description">${description}</div>`: ''}
                     ${!disableDescription? 
                         html`<hr><div class="description install-description">${installDescription? installDescription: `${msg('This site has app functionality.')} ${msg('Install it on your device for extensive experience and easy access.')}`}</div>` 
                         : ''}
                     ${screenshotsAvailable && installAvailable? html`<pwa-gallery .screenshots=${manifest.screenshots as ManifestScreenshot[]} .rtl="${isRTL}"></pwa-gallery>`: ''}
-                    <div class="action-buttons">
+                    <div class="action-buttons" part="action-buttons">
                         ${screenshotsAvailable? html`<button class="material-button secondary" @click='${toggleGallery}'>${galleryRequested?msg('Less'):msg('More')}</button>`:''}
                         <button class="material-button primary install" @click='${install}'>${msg('Install')}</button>
                     </div>
